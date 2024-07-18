e50358d28433ebb190d826435e3a46fd7d1ed8fa
nathius262 <egbodonathaniel@yahoo.com>
2024-07-18 15:57:58 +0100

    bug fix pagination

--- blog/api/views.py
+++ blog/api/views.py
@@ -10,7 +10,7 @@ from django.db.models import Count
 
 # page size
 class SetBlogPostPaginationResult(PageNumberPagination):
-    page_size = 20
+    page_size = 10
     page_size_query_param = 'page_size'
     max_page_size = 1000
 
