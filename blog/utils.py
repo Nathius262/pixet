9f35ab02032473461af72ff8851d7f92ae77650c
nathius262 <egbodonathaniel@yahoo.com>
2024-07-18 11:14:39 +0100

    bug fix- image duplicates

--- blog/utils.py
+++ blog/utils.py
@@ -4,10 +4,13 @@ import os
 
 
 def upload_location(instance, filename):
+    # Generate the file path based on the instance's author ID and slug
     file_path = 'blog/user_{author_id}/{slug}_post.jpeg'.format(
-        author_id=str(instance.author.id), slug=str(instance.id), filename=filename
+        author_id=str(instance.author.id), slug=str(instance.slug)
     )
+    # Get the full path to the file
     full_path = os.path.join(settings.MEDIA_ROOT, file_path)
+    # If the file already exists, remove it
     if os.path.exists(full_path):
         os.remove(full_path)
     return file_path
