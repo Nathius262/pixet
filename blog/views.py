66f0d53fd5f761dce6d800b5cc60ecf302d4a9ba
Nathius262 <egbodonathaniel@yahoo.com>
2024-08-08 23:11:24 +0100

    test

--- blog/views.py
+++ blog/views.py
@@ -1,30 +1,38 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from cloudinary.uploader import upload
-from cloudinary.utils import cloudinary_url
-from cloudinary.uploader import upload_image
 import os
+from decouple import config
+
+import cloudinary
+import cloudinary.uploader
+from cloudinary.utils import cloudinary_url
+
+# Configuration       
+cloudinary.config(
+    cloud_name=config('CLOUD_NAME'),
+    api_key=config('API_KEY'),
+    api_secret=config('API_SECRET'),
+    secure=True,
+    #api_proxy = "http://proxy.server:3128"
+)
+
 
 def ckeditor5_upload(request):
     if request.method == "POST":
-        # Bypass the proxy for the Cloudinary API
-        os.environ['NO_PROXY'] = 'api.cloudinary.com'
 
         print(request.FILES)
         uploaded_file = request.FILES['upload']
         
-        # Upload the file directly to Cloudinary
-        upload_result = upload_image(
-            uploaded_file,
-            folder="blog_images/",  # Your desired folder path
-        )
-        
-        # Generate the URL for the uploaded image
-        url, options = cloudinary_url(upload_result['public_id'], format="jpg")
+        # Upload an image
+        upload_result = cloudinary.uploader.upload(uploaded_file)
+        url = upload_result["secure_url"]
+        print(url)
         
         return JsonResponse({
             "url": url,
         })
-# Create your views here.
+        
+
+
 def index_view(request):
     return redirect('/admin/')
